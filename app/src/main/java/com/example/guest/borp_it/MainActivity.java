5324715e4aea08541f13a0341f7e48e57e9807b7
Abigail Rolling and David Remington <david.ian.remington@gmail.com>
2016-05-09 10:59:37 -0700

    create layout for gameactivity

--- app/src/main/java/com/example/guest/borp_it/MainActivity.java
+++ app/src/main/java/com/example/guest/borp_it/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.guest.borp_it;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.startButton) Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
