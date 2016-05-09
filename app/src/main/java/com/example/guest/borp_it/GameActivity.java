93d1cacac841d9fe63e0c7f7ee54822471fbfe12
Abigail Rolling and David Remington <david.ian.remington@gmail.com>
2016-05-09 11:42:30 -0700

    Add onlongclicklistener with player accuracy checking

--- app/src/main/java/com/example/guest/borp_it/GameActivity.java
+++ app/src/main/java/com/example/guest/borp_it/GameActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -34,6 +35,20 @@ public class GameActivity extends AppCompatActivity {
         mRandom = new Random();
         mPrompts = new String[] {"Borp", "Pull", "Twist"};
         generatePrompt();
+
+        mBorpButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if(mCurrentPrompt.equals("Borp")){
+                    Log.v(TAG, "correct Borp");
+                    generatePrompt();
+                    return true;
+                }
+                Log.v(TAG, "incorrect Borp");
+                generatePrompt();
+                return false;
+            }
+        });
 
     }
 
